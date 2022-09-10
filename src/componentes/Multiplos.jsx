4f06af657165ce6b93512aae6870da56be02df42
EliasFK <eliasfrank58@gmail.com>
2022-09-10 20:59:10 -0300

    estudo sobre construtor

--- src/componentes/Multiplos.jsx
+++ src/componentes/Multiplos.jsx
@@ -4,6 +4,5 @@ export const BoaTarde = props => <h1>Boa Tarde {props.nome}!</h1>
 
 export const BoaNoite = props => <h1>Boa Noite {props.nome}!</h1>
 
-export {BoaTarde, BoaNoite}
-
 export default {BoaTarde, BoaNoite}
+
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM  from 'react-dom'
 //import Primeiro from './componentes/Primeiro'
 //import BomDia from './componentes/BomDia'
-import Multi, { BoaNoite } from './componentes/Multiplos'
+//import Multi, { BoaNoite } from './componentes/Multiplos'
+import Saudacao from './componentes/Saudacao'
+
 
 
 ReactDOM.render(
     <div>
-        <Multi.BoaTarde nome = 'Elias'/>
-        <BoaNoite nome = 'Elizeu'/>
+        <Saudacao tipo = "Bom dia" nome = "Elias"/>
+        {/* <Multi.BoaTarde nome = 'Elias'/>
+        <BoaNoite nome = 'Elizeu'/> */}
     </div>
     ,document.getElementById('root'))
 //ReactDOM.render(<BomDia nome="Elizeu" idade={25}/>, document.getElementById('root'))
