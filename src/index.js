c3d7d06ffc253e3864b9d9f550f2afc1ace1c3dd
EliasFK <eliasfrank58@gmail.com>
2022-09-10 22:32:53 -0300

    mais estudos

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM  from 'react-dom'
 //import Primeiro from './componentes/Primeiro'
 //import BomDia from './componentes/BomDia'
 //import Multi, { BoaNoite } from './componentes/Multiplos'
-import Saudacao from './componentes/Saudacao'
-
-
+//import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>
-        <Saudacao tipo = "Bom dia" nome = "Elias"/>
+        <Pai nome='Edson' sobrenome='Frank'>
+        <Filho nome='Elias' />
+        <Filho nome='Elizeu' />
+        <Filho nome='Nayra' />
+        </Pai>
+        {/* <Saudacao tipo = "Bom dia" nome = "Elias"/> */}
         {/* <Multi.BoaTarde nome = 'Elias'/>
         <BoaNoite nome = 'Elizeu'/> */}
     </div>
